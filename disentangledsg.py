545a43f85c57649392e9b60154961c7fe9efc4c6
Sebastian Zimmermann <sebastian.zimmermann@campus.tu-berlin.de>
2022-05-29 17:18:00 +0200

    modularized training steps

--- disentangledsg.py
+++ disentangledsg.py
@@ -60,8 +60,15 @@ import pytorch_lightning as pl
 from torch.nn import ModuleList
 
 from defaultvalues import optim_conf
-from util import requires_grad, mixing_noise, d_logistic_loss
+from util import (
+    requires_grad,
+    mixing_noise,
+    d_logistic_loss,
+    d_r1_loss,
+    g_nonsaturating_loss,
+)
 from non_leaking import augment, AdaptiveAugment
+
 
 
 
@@ -73,6 +80,7 @@ class DisentangledSG(pl.LightningModule):
         generator,
         encoder,
         discriminator,
+        args,
         optim_conf=optim_conf):
 
         super().__init__()
@@ -89,10 +97,16 @@ class DisentangledSG(pl.LightningModule):
                 self.discriminator,
         ]
 
-        self.latent  = mappingnetwork.style_dim
+        device = f"cuda:{args.gpu}"
+        self.ada_augment = AdaptiveAugment(args.ada_target, args.ada_length, 8, device)
+
         self.imgsize = generator.size
 
-        self.augment_data = False
+        self.args = args
+
+        self.mean_path_length = 0
+
+        self.loss_dict = {}
 
         # TODO: flexible downstream tasks (as a list)
         #self.downstream = None
@@ -125,72 +139,154 @@ class DisentangledSG(pl.LightningModule):
 
 
     def training_step(self, batch, batch_idx, optimizer_idx):
-        # batch contains x, y (data and label)
-        # configure pytorch lightning for multistage training steps:
-        # https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html?highlight=training_step#training-step
 
         if optimizer_idx == 0:
-            # OPTIMIZE DISCRIMINATION
-    
-            self.set_trainable(
-                    self.discriminator,
-                    self.encoder)
-
-            real_img_batch = batch[0]
-            batch_size = real_img_batch.shape[0]
-
-            noise = mixing_noise(batch_size, self.latent, args.mixing)
-            fake_img_batch, _ = self.generator([mapping(z) for z in noise])
-
-            if self.augment_data:
-                real_img_aug, _ = augment(real_img_batch, ada_aug_p)
-                fake_img, _ = augment(fake_img_batch, ada_aug_p)
-
-            else:
-                real_img_aug = real_img_batch
-
-            fake_pred = self.discriminator(self.encoder(fake_img))
-            real_pred = self.discriminator(self.encoder(real_img_aug))
-            d_loss = d_logistic_loss(real_pred, fake_pred)
-
-            # TODO: implement the rest of solver_mnist.py
-            # (around line 200)
-
-            # TODO:
-            # do we use *automatic optimization* or *manual optimization*?
-            # to enable manual optimization, add the following to the object:
-            # self.automatic_optimization = False
-            #
-            # see also here:
-            # https://pytorch-lightning.readthedocs.io/en/latest/common/optimization.html#id2
-
-            # TODO:
-            # what is the return value?
-            # either a loss tensor or a dictionary, which can
-            # have any number of entries but *must* have a
-            # "loss" entry.
-            #
-            # see also here:
-            # https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#training-step
-
-
+            return self.optimize_discrimination(batch, batch_idx)
         if optimizer_idx == 1:
-            # OPTIMIZE GENERATION
-            # TODO
-            self.set_trainable(
-                    self.mapping,
-                    self.generator)
-            pass
-
+            return self.optimize_generation(batch, batch_idx)
         if optimizer_idx == 2:
-            # OPTIMIZE CONSISTENCY
-            # TODO
-            self.set_trainable(
-                    self.mapping,
-                    self.generator,
-                    self.encoder)
-            pass
+            return self.optimize_consistency(batch, batch_idx)
 
+        # we should never arrive here
+        return None
+
+
+    def optimize_discrimination(self, batch, batch_idx):
+        # check for regularisation interval
+        d_regularize = batch_idx % self.args.d_reg_every == 0
+        if d_regularize:
+            return self.regularize_discrimination(batch)
+        # default: do normal optimisation
+        return self.optimize_enc_and_disc(batch)
+
+
+    def optimize_enc_and_disc(self, batch):
+        self.set_trainable(
+                self.encoder,
+                self.discriminator)
+
+        real_img   = batch[0]
+        batch_size = real_img.shape[0]
+
+        noise = mixing_noise(batch_size, self.args.latent, self.args.mixing)
+        fake_img, _ = self.generator([self.mapping(z) for z in noise])
+
+        if self.args.augment:
+            real_img_aug, _ = augment(real_img, self.ada_augment.ada_aug_p)
+            fake_img, _ = augment(fake_img, self.ada_augment.ada_aug_p)
+
+        else:
+            real_img_aug = real_img
+
+        fake_pred = self.discriminator(self.encoder(fake_img))
+        real_pred = self.discriminator(self.encoder(real_img_aug))
+        d_loss = d_logistic_loss(real_pred, fake_pred)
+
+        self.loss_dict["d"] = d_loss
+        self.loss_dict["real_score"] = real_pred.mean()
+        self.loss_dict["fake_score"] = fake_pred.mean()
+
+        if self.args.augment and self.args.augment_p == 0:
+            self.ada_augment.tune(real_pred)
+
+        return {"loss": d_loss}
+
+
+    def regularize_discrimination(self, batch):
+        real_img = batch[0]
+        self.set_trainable(
+                self.encoder,
+                self.discriminator,
+                real_img)
+
+        if self.args.augment:
+            real_img_aug, _ = augment(real_img, self.ada_augment.ada_aug_p)
+
+        else:
+            real_img_aug = real_img
+
+        real_pred = self.discriminator(self.encoder(real_img_aug))
+        r1_loss = d_r1_loss(real_pred, real_img)
+
+        reg_loss = (self.args.r1 / 2 * r1_loss * self.args.d_reg_every + 0 * real_pred[0])
+        self.loss_dict["r1"] = r1_loss
+
+        return {"loss": reg_loss}
+
+
+    def optimize_generation(self, batch, batch_idx):
+        # check for regularisation interval
+        g_regularize = batch_idx % self.args.g_reg_every == 0
+        if g_regularize:
+            return self.regularize_generation(batch)
+        # default: do normal optimisation
+        return self.optimize_map_and_gen(batch)
+
+
+    def optimize_map_and_gen(self, batch):
+        self.set_trainable(
+                self.mapping,
+                self.generator)
+
+        real_img   = batch[0]
+        batch_size = real_img.shape[0]
+
+        noise = mixing_noise(batch_size, self.args.latent, self.args.mixing)
+        fake_img, _ = self.generator([self.mapping(z) for z in noise])
+
+        if self.args.augment:
+            fake_img, _ = augment(fake_img, self.ada_augment.ada_aug_p)
+
+        fake_pred = self.discriminator(self.encoder(fake_img))
+        g_loss = g_nonsaturating_loss(fake_pred)
+
+        self.loss_dict["g"] = g_loss
+
+        return {"loss": g_loss}
+
+
+    def regularize_generation(self, batch, batch_idx):
+
+        batch_size = batch[0].shape[0]
+
+        path_batch_size = max(1, batch_size // self.args.path_batch_shrink)
+        noise = mixing_noise(path_batch_size, self.args.latent, self.args.mixing)
+        fake_img, latents = self.generator([self.mapping(z) \
+                for z in noise], return_latents=True)
+
+        path_loss, self.mean_path_length, path_lengths = g_path_regularize(
+            fake_img, latents, self.mean_path_length
+        )
+
+        weighted_path_loss = self.args.path_regularize * self.args.g_reg_every * path_loss
+        if self.args.path_batch_shrink:
+            weighted_path_loss += 0 * fake_img[0, 0, 0, 0]
+
+        self.loss_dict["path"] = path_loss
+        self.loss_dict["path_length"] = path_lengths.mean()
+
+        return {"loss": weighted_path_loss}
+
+
+    def optimize_consistency(self, batch, batch_idx):
+
+        real_img   = batch[0]
+        batch_size = real_img.shape[0]
+
+        self.set_trainable(
+                self.generator,
+                self.mapping,
+                self.encoder)
+
+        batch_size = batch[0].shape[0]
+
+        w_z = self.mapping(torch.randn(batch_size, self.args.latent))
+        fake_img, _ = self.generator([w_z])
+
+        consistency_loss = (w_z - self.encoder(fake_img)).pow(2).mean()
+        self.loss_dict["consistency_loss"] = consistency_loss
+
+        return {"loss": consistency_loss}
 
 
     def configure_optimizers(self):
--- util.py
+++ util.py
@@ -4,7 +4,7 @@ import functools
 import operator
 
 import torch
-from torch import nn
+from torch import nn, autograd
 from torch.nn import functional as F
 from torch.autograd import Function
 
@@ -478,8 +478,6 @@ def make_noise(batch, latent_dim, n_noise):
 
 
 def mixing_noise(batch, latent_dim, prob):
-    """
-    """
     if prob > 0 and random.random() < prob:
         return make_noise(batch, latent_dim, 2)
 
@@ -492,4 +490,38 @@ def d_logistic_loss(real_pred, fake_pred):
     fake_loss = F.softplus(fake_pred)
 
     return real_loss.mean() + fake_loss.mean()
+
+
+def d_r1_loss(real_pred, real_img):
+    with conv2d_gradfix.no_weight_gradients():
+        grad_real, = autograd.grad(
+            outputs=real_pred.sum(),
+            inputs=real_img,
+            create_graph=True
+        )
+    grad_penalty = grad_real.pow(2).reshape(grad_real.shape[0], -1).sum(1).mean()
+
+    return grad_penalty
+
+
+def g_nonsaturating_loss(fake_pred):
+    loss = F.softplus(-fake_pred).mean()
+
+    return loss
+
+
+def g_path_regularize(fake_img, latents, mean_path_length, decay=0.01):
+    noise = torch.randn_like(fake_img) / math.sqrt(
+        fake_img.shape[2] * fake_img.shape[3]
+    )
+    grad, = autograd.grad(
+        outputs=(fake_img * noise).sum(), inputs=latents, create_graph=True
+    )
+    path_lengths = torch.sqrt(grad.pow(2).sum(2).mean(1))
+
+    path_mean = mean_path_length + decay * (path_lengths.mean() - mean_path_length)
+
+    path_penalty = (path_lengths - path_mean).pow(2).mean()
+
+    return path_penalty, path_mean.detach(), path_lengths
 
