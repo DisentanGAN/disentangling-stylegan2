ff55dc44e1a7a30380fa5a7ebb81cc37886259bf
Lorenz <lorenz.hufe@web.de>
2022-06-20 20:20:21 +0200

    formatting

--- classifier.py
+++ classifier.py
@@ -1,12 +1,37 @@
 import torch.nn as nn
 
+
 class LinearClassifier(nn.Module):
-    
+
     def __init__(self, in_dim, out_dim):
         super().__init__()
-        self.layer = nn.Linear(in_features=in_dim, out_features=out_dim)
-        
+        self.layer = nn.Linear(in_dim, out_dim)
+
     def forward(self, x):
         x = self.layer(x)
         x = nn.functional.softmax(x, dim=1)
         return x
+
+
+# class AdaptiveClassifier(nn.Module):
+#     def __init__(self, in_dim, out_dim, channels):
+#         super().__init__()
+#         self.layers = nn.ModuleList()
+        
+#         self.layers.append(
+#             nn.Linear(in_dim, channels[0]))
+#         for i in range(len(channels) - 1):
+#             self.layers.append(
+#                 nn.Linear(channels[i], out_features=channels[i+1]))
+
+#         self.classification = nn.Linear(channels[-1], out_dim)
+
+#     def forward(self, x):
+#         for layer in self.layers:
+#             x = layer(x)
+#             x = nn.functional.relu(x)
+        
+#         x = self.classification(x)
+#         x = nn.functional.softmax(x, dim=1)
+
+#         return x
